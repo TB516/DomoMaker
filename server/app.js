cd8d2f67236bc33044032b9566b1b2638103f902
TB516 <tberrios9800@gmail.com>
2024-11-12 01:31:00 -0500

    Part C

--- server/app.js
+++ server/app.js
@@ -7,6 +7,10 @@ const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
 const helmet = require('helmet');
 const session = require('express-session');
+const RedisStore = require('connect-redis').default;
+const redis = require('redis');
+
+require('dotenv').config();
 
 const router = require('./router');
 
@@ -20,27 +24,39 @@ mongoose.connect(dbURI).catch((e) => {
   }
 });
 
-const app = express();
+const redisClient = redis.createClient({
+  url: process.env.REDISCLOUD_URL,
+});
 
-app.use(helmet());
-app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
-app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
-app.use(compression());
-app.use(bodyParser.urlencoded({ extended: true }));
-app.use(bodyParser.json());
-app.use(session({
-  key: 'sessionid',
-  secret: 'Domo Arigato',
-  resave: false,
-  saveUninitialized: false,
-}));
-app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
-app.set('view engine', 'handlebars');
-app.set('views', `${__dirname}/../views`);
+redisClient.on('error', (e) => console.log('Redis Client Error', e));
 
-router(app);
+redisClient.connect().then(() => {
+  const app = express();
 
-app.listen(port, (e) => {
-  if (e) throw e;
-  console.log(`Listening on port ${port}`);
+  app.use(helmet());
+  app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
+  app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(bodyParser.json());
+  app.use(session({
+    key: 'sessionid',
+    store: new RedisStore({
+      client: redisClient,
+    }),
+    secret: 'Domo Arigato',
+    resave: false,
+    saveUninitialized: false,
+  }));
+  app.engine('handlebars', expressHandlebars.engine({ defaultLayout: '' }));
+  app.set('view engine', 'handlebars');
+  app.set('views', `${__dirname}/../views`);
+
+  router(app);
+
+  app.listen(port, (e) => {
+    if (e) throw e;
+    console.log(`Listening on port ${port}`);
+    console.log(process.env.NODE_ENV);
+  });
 });
